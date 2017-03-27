c40440e8c21f20da5604d8529209da2486410f51
Rhyza Velasco <rhyza.velasco@gmail.com>
2017-03-26 21:31:17 -0400

    Update Modify User form and queries
    Added full modify user form to admin page.
    Added outline of form functionality.

--- 4707 Project 1/admin.php
+++ 4707 Project 1/admin.php
@@ -25,25 +25,26 @@ if (isset($_COOKIE[$cookie_name])) {
     sys_error("You are not authorized to view this page.");
 }
 
-if (isset($_REQUEST['act']) == 'addUser') {
-    if (strlen($_REQUEST['username']) > 3 && strlen($_REQUEST['password1']) > 5 && $_REQUEST['password1'] == $_REQUEST['password2']) {
-        $userhash = hash('sha256', $_POST['username']);
-        $sql = "SELECT * FROM users WHERE username = '".$userhash."'";
-        $result = $conn->query($sql);
-        if ($result->num_rows <= 0) {
-            $passhash = hash('sha256', $_POST['password1']);
-            if (empty($_REQUEST['type'])) {
-                $type = "user";
-            } else {
-                $type = $_REQUEST['type'];
-            }
+if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'addUser') {
+    if (strlen($_REQUEST['username']) > 3 && strlen($_REQUEST['newpw1']) > 5 && $_REQUEST['newpw1'] == $_REQUEST['newpw2']) {
+    	if (empty($_REQUEST['newtype']) || strtolower($_REQUEST['newtype'] != "user" || strtolower($_REQUEST['newtype'] != "admin") {
+            sys_error("User type not specified.");
+        } else {
+            $type = strtolower($_REQUEST['newtype']);
+            $userhash = hash('sha256', $_POST['username']);
+            $sql = "SELECT * FROM users WHERE username = '".$userhash."'";
+            $result = $conn->query($sql);
+            if ($result->num_rows <= 0) {
+                $passhash = hash('sha256', $_POST['newpw1']);
+            
 
-            $sql2 = "INSERT INTO users (username, password, salt, type, failedlogins) VALUES ('".$userhash."', '".$passhash."', ".time().", '".$type."', 0);";
-            $result2 = $conn->query($sql2);
-            // Success
-            if ($result2) {
-            	include "$template_dir/pageheader.html";
-                echo "<div class='alert alert-success' role='alert'><strong>New ".$type." successfully created.</strong></div>";
+                $sql2 = "INSERT INTO users (username, password, salt, type, failedlogins) VALUES ('".$userhash."', '".$passhash."', ".time().", '".$type."', 0);";
+                $result2 = $conn->query($sql2);
+                // Success
+                if ($result2) {
+                	include "$template_dir/pageheader.html";
+                    echo "<div class='alert alert-success' role='alert'><strong>New ".$type." successfully created.</strong></div>";
+                }
             }
         } else {
             sys_error("User already exists.");
@@ -51,21 +52,37 @@ if (isset($_REQUEST['act']) == 'addUser') {
     } else {
         sys_error("Invalid username or password.");
     }
-} else {
-    if (isset($_COOKIE[$cookie_name])) {
-        echo "<p>".$_COOKIE[$cookie_name]."</p>";
-    }
-}
-
-if (isset($_REQUEST['act']) == 'findUser') {
+} else if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'modUser') {
     if (!empty($_REQUEST['username'])) {
         $userhash = hash('sha256', $_POST['username']);
         $sql = "SELECT username FROM users WHERE username = '".$userhash."'";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
-            include "$template_dir/pageheader.html";
-            echo "<h3>Modifying info for user ".$_POST['username']."</h3>";
-            include "$template_dir/accountadmin.html";
+        	// change password
+            if (!empty($_REQUEST['pw1'])) {
+            	if (strlen($_REQUEST['pw1']) > 5 &&  $_REQUEST['pw1'] == $_REQUEST['pw2']) {
+            		$pwsql = "";
+            		$pwresult = $conn->query($pwsql);
+            	} else {
+            		sys_error("Invalid password.");
+            	}
+            }
+
+            // change type
+            if (!empty($_REQUEST['modtype'])) {
+            	if (strtolower($_REQUEST['newtype'] == "user" || strtolower($_REQUEST['newtype'] == "admin") {
+            		$typesql = "";
+            		$typeresult = $conn->query($typesql);
+            	} else {
+            		sys_error("Invalid user type.");
+            	}
+            }
+
+            // delete user
+            if ($_REQUEST['delete'] == "DELETE") {
+            	$delsql = "";
+            	$delresult = $conn->query($delsql);
+            }
         } else {
             sys_error("User ".$_POST['username']." does not exist.");
         }
@@ -77,4 +94,5 @@ if (isset($_REQUEST['act']) == 'findUser') {
         echo "<p>".$_COOKIE[$cookie_name]."</p>";
     }
 }
+
 ?>
